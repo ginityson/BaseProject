4751ce9a1d5d97594c7519170314e9fa5377d90f
ginityson <ginityson@yahoo.com>
2016-04-26 16:59:45 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//Author Gini Tyson
+//Creation Date 4/26/2016
+baseProject = {
+    name: "baseProject",
+    author: "Gini Tyson",
+    creationDate: "4/26/16"
+};
+console.log(baseProject);
+
+
+
+
+
+
