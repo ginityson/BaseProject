61a14e21df188eb7751a6f69d9fffa3b8402a488
ginityson <ginityson@yahoo.com>
2016-04-26 17:26:09 -0500

    Added files via upload

---
+++ Readme.js
@@ -0,0 +1,8 @@
+//Readme.js
+//baseProject repo contains
+    //index.html that hopefully contains base functionality for an html page
+    //script.js is my javascript file with documentation
+    //stylesheet.css where i may have colored the words blue
+
+
+
